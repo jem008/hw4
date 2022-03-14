309eb850f057b3e6f64e6fd686e832ffbbb88662
jem008 <joe.mcnulty@kellogg.northwestern.edu>
2022-03-14 23:08:10 +0000

    commit and sync

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    entered_password = params["password"]
+    @user = User.find_by({email: entered_email})
+    if @user
+      if BCrypt::Password.new(@user.password) == entered_password
+        redirect_to "/places"
+    else
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
